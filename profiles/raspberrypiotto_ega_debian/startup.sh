78dc61ce18447d8a1db52c8ae861345c17ae77d8
edgar <32952693+Yongyiphan@users.noreply.github.com>
2026-02-07 16:35:00 +0800

    updated python lsp support

--- profiles/raspberrypiotto_ega_debian/startup.sh
+++ profiles/raspberrypiotto_ega_debian/startup.sh
@@ -33,3 +33,14 @@ start() {
     echo "No virtual environment found in current or parent directories."
     return 1
 }
+
+restart_ssh() {
+    echo "Restarting SSH Agent..."
+
+    # Detect if inside a tmux session
+    if [ -n "$TMUX" ]; then
+        echo "Detected inside a tmux session."
+        eval $(tmux show-env -s SSH_AUTH_SOCK)
+        echo "✓ SSH_AUTH_SOCK reassigned in tmux session: $SSH_AUTH_SOCK"
+    fi
+}
